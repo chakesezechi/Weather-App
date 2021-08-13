129e318b5c48e1d7b0285460cbfffed98dadc6c2
Chekwube Ezechi <zechirayjr@yahoo.com>
2021-08-13 23:23:20 +0100

    added function for the day, date, month and year

--- script.js
+++ script.js
@@ -53,3 +53,16 @@ function displayData(response) {
   }
 }
 
+function dateFunction(d) {
+  let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+
+  return `${day}, ${date} ${month} ${year}`;
+}
