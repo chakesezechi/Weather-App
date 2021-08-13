040677fb62f0e33378a85326f95af7b797dad7fb
Chekwube Ezechi <zechirayjr@yahoo.com>
2021-08-13 22:40:59 +0100

    added function to display the data to the ui

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ function getData() {
     }).then(displayData);
 }
 
+function displayData(response) {
+  console.log(response);
+  if (response.cod === "404"){
+    const error = document.querySelector(".error");
+    error.textContent = "Please enter a valid city";
+    search.value = "";
+  } else {
+    const city = document.querySelector(".city");
+    city.innerText =`${response.name}, ${response.sys.country}`;
+
+    const today = new Date();
+    const date = document.querySelector(".date");
+    date.innerText = dateFunction(today);
+
+    const temp = document.querySelector(".temp");
+    temp.innerHTML = `Temp: ${Math.round(response.main.temp)} <span>°C</span>`;
+
+    const weather = document.querySelector(".weather");
+    weather.innerText = `Weather: ${response.weather[0].main}`;
+
+    const tempRange = document.querySelector(".temp-range");
+    tempRange.innerText = `Temp Range: ${Math.round(response.main.temp_min)}°C / ${Math.round(response.main.temp_max)}°C`;
+
+    const weatherIcon = document.querySelector(".weather-icon");
+    const iconURL= "https://api.openweathermap.org/img/w/";
+    weatherIcon.src = iconURL + response.weather[0].icon + ".png";
+
+    search.value = "";
+  }
+}
+
